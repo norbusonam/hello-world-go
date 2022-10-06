3d2d89c22c48ab6c89b75292dd48bb45f2dcb3c3
norbusonam <nsonam1822@gmail.com>
2022-10-05 22:22:41 -0700

    function fun

---
+++ 02 functions/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 6
+	var y int = 3
+	fmt.Println(add(x, y))
+	fmt.Println(subtract(x, y))
+	fmt.Println(multiply(x, y))
+}
+
+// basic
+func add(x int, y int) int {
+	return x + y
+}
+
+// simplified params
+func subtract(x, y int) int {
+	return x - y
+}
+
+// simplified params and named return value
+func multiply(x, y int) (product int) {
+	product = x * y
+	return
+}
