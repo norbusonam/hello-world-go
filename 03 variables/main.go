9727a092961c7d5e5615a581a210686e21b1c986
norbusonam <nsonam1822@gmail.com>
2022-10-06 20:36:35 -0700

    constants (and some formatting)

--- 03 variables/main.go
+++ 03 variables/main.go
@@ -8,13 +8,14 @@ func main() {
 	var x int = 1
 	var y = 1
 	z := 1
-
 	fmt.Println(x, y, z)
 
 	var i int    // default to 0
 	var j string // default to ""
 	var k bool   // default to false
-
 	fmt.Println(i, j, k)
+
+	const name = "Norbu" // constants can not be rename (obviously :P)
+	fmt.Println(name)
 
 }
